fddcf282313071f99df6314ebec9d996c5e9728a
Harlan Cleiton <harlancleiton@gmail.com>
2019-07-13 22:37:00 -0300

    MeatServer

--- src/app/meat.server.ts
+++ src/app/meat.server.ts
@@ -1,9 +1,43 @@
 import * as restify from 'restify';
 
-export class Server {
-    private _server: Server;
+class MeatServer {
+    // tslint:disable-next-line: variable-name
+    private _application: restify.Server | undefined;
 
-    constructor(){
-        this._server = 
+    get application(): restify.Server {
+        return this._application!;
     }
+
+    public bootstrap(): Promise<MeatServer> {
+        return this.initRoutes().then(() => this);
+    }
+
+    private initRoutes(): Promise<restify.Server> {
+        return new Promise<restify.Server>((resolve, reject) => {
+            try {
+
+                this._application = restify.createServer({
+                    name: 'Meat API',
+                    version: '1.0.0'
+                });
+
+                this._application.use(restify.plugins.bodyParser());
+
+                this._application.get('/hello', (req, res, next) => {
+                    res.json({ message: 'Hello World' });
+                    return next();
+                });
+
+                this._application.listen(3000, () => {
+                    resolve(this._application);
+                });
+
+            } catch (error) {
+                reject(error);
+            }
+        });
+    }
+
 }
+
+export default new MeatServer();
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
+import meatServer from './app/meat.server';
+
 console.log('Hello World');
+
+meatServer.bootstrap()
+    .then((server) => {
+        console.log('Server is listening on: ', server.application.address());
+    }).catch((err) => {
+        console.log('Erro failed to start');
+        console.error(err);
+        process.exit(1);
+    });
