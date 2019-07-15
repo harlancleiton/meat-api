0754cf83a9c78a0537b2a24023325e58ab037b91
Harlan Cleiton <harlancleiton@gmail.com>
2019-07-14 23:23:59 -0300

    Router: Render method

--- src/app/server/router.ts
+++ src/app/server/router.ts
@@ -1,6 +1,20 @@
+import { EventEmitter } from 'events';
+import * as mongoose from 'mongoose';
 import * as restify from 'restify';
 import { IRouter } from '../interfaces/router.interface';
 
-export abstract class Router implements IRouter {
+export abstract class Router extends EventEmitter implements IRouter {
     public abstract applyRouter(application: restify.Server): void;
+
+    public render(req: restify.Request, res: restify.Response, next: restify.Next): any {
+        return (document: mongoose.Document): void => {
+            if (document) {
+                this.emit('beforeRender', document);
+                res.json(document);
+            } else {
+                res.send(404);
+            }
+            return next();
+        };
+    }
 }
--- src/app/users/user.router.ts
+++ src/app/users/user.router.ts
@@ -4,72 +4,54 @@ import { Router } from '../server/router';
 import { IUser, userModel } from './user.model';
 
 class UserRouter extends Router {
+
+    constructor() {
+        super();
+        this.on('beforeRender', (document: mongoose.Document | IUser | any) => {
+            if (document.password) {
+                document.password = undefined;
+            }
+        });
+    }
+
     public applyRouter(application: restify.Server): void {
 
         application.get('/users', (req, res, next) => {
-            userModel.find().exec().then((users: IUser[]) => {
-                res.json(users);
-                return next();
-            });
+            userModel.find().exec().then(this.render(req, res, next));
         });
 
         application.get('/users/:id', (req, res, next) => {
-            userModel.findById(req.params.id).exec().then((user: IUser | null) => {
-                res.json(user);
-                return next();
-            });
+            userModel.findById(req.params.id).exec().then(this.render(req, res, next));
         });
 
         application.post('/users', (req, res, next) => {
-            userModel.create(req.body).then((user: IUser) => {
-                user.password = undefined;
-                res.json(user);
-                return next();
-            });
+            userModel.create(req.body).then(this.render(req, res, next));
         });
 
-        application.put('/users/:id', (req, resp, next) => {
+        application.put('/users/:id', (req, res, next) => {
             const options: mongoose.ModelUpdateOptions = { overwrite: true, runValidators: true };
             userModel.update({ _id: req.params.id }, req.body, options)
+                .exec().catch((err) => { console.log('Failed'); })
                 // @ts-ignore
-                .exec().then((query: mongoose.Query<any>) => {
+                .then((query: mongoose.Query<any>) => {
                     // @ts-ignore
                     if (query.n) {
                         return userModel.findById(req.params.id).exec();
                     } else {
-                        resp.send(404);
+                        res.send(404);
                     }
-                }).then((user: IUser | null) => {
-                    resp.json(user);
-                    return next();
-                });
+                }).then(this.render(req, res, next));
         });
 
         application.patch('/users/:id', (req, res, next) => {
             const options: mongoose.QueryFindOneAndUpdateOptions = { new: true, runValidators: true };
             userModel.findByIdAndUpdate(req.params.id, req.body, options).exec()
-                .then((user: IUser | null) => {
-                    if (user) {
-                        res.json(user);
-                        return next();
-                    } else {
-                        res.send(404);
-                        return next();
-                    }
-                });
+                .then(this.render(req, res, next));
         });
 
         application.del('/users/:id', (req, res, next) => {
             userModel.findByIdAndDelete(req.params.id).exec()
-                .then((user: IUser | null) => {
-                    if (user) {
-                        res.send(204);
-                        return next();
-                    } else {
-                        res.send(404);
-                        return next();
-                    }
-                });
+                .then(this.render(req, res, next));
         });
 
     }
